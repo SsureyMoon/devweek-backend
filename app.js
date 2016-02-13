18bbf2271b8cfc3c4e836e57df2b4c29a48e9161
Ssurey Moon <ssureymoon@gmail.com>
2016-02-13 15:06:31 -0800

    fix bug

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var app = express();
 app.use(logger('developement'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
