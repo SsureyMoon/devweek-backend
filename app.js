84a023cfc44a2c1804845d0a3f6c10d24af698c4
Michael Campos <camposmj89@gmail.com>
2016-02-13 19:59:44 -0800

    Resolved merge conflict in app.js

--- app.js
+++ app.js
@@ -3,26 +3,8 @@ var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var http = require('http');
-redis   = require('redis').createClient;
 
 var config  = require('./config');
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
-var redis = require('redis').createClient({
-    host: config.redis.host,
-    port: config.redis.port,
-    password: config.redis.password
-});
-
-
-=======
-var adapter = require('socket.io-redis');
->>>>>>> 6bb80951f1618043b2bb97951693fd6e4a3f8664
-
-
->>>>>>> 2cef0abf8187a524c1f79ee7400911da26824ec6
 var routes = require('./routes');
 
 var app = express();
@@ -34,33 +16,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var pub = redis({
-    key: config.socket.namespace,
-    host: config.redis.host,
-    port: config.redis.port,
-    auth_pass: config.redis.password
-});
-var sub = redis({
-    key: config.socket.namespace,
-    host: config.redis.host,
-    port: config.redis.port,
-    auth_pass: config.redis.password
-});
-
-var adapter = adapter({pubClient: pub, subClient:sub});
-
-var redisClient = redis(config.redis.port, config.redis.host);
-redisClient.auth(config.redis.password);
-
-
 var socketHandler = require('./lib/socket')
-    .init(server, redisClient, adapter, config.socket.namespace);
+    .init(server, config.socket.namespace);
 
 app.use('/', routes);
 
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
-
+    
     err.status = 404;
     next(err);
 });
@@ -96,9 +59,4 @@ if (require.main === module) {
     exports.port = port;
 }
 
-<<<<<<< HEAD
 exports.app = app;
-=======
-exports.redisClient = redisClient
-exports.app = app;
->>>>>>> 6bb80951f1618043b2bb97951693fd6e4a3f8664
